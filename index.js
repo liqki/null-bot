97b0c78b37f9ea261519970acf565bd721b341e8
BuildTools <leda.bonfert@gmail.com>
2023-02-22 19:01:33 +0100

    replit doesn't work anymore

--- index.js
+++ index.js
@@ -56,18 +56,3 @@ const destroy = () => {
 };
 process.on("SIGINT", destroy);
 process.on("SIGTERM", destroy);
-
-const keepAlive = require("./server");
-const Monitor = require("ping-monitor");
-
-keepAlive();
-const monitor = new Monitor({
-  website: "",
-  title: "NAME",
-  interval: 2,
-});
-
-monitor.on("up", (res) => console.log(`${res.website} its on.`));
-monitor.on("down", (res) => console.log(`${res.website} it has died - ${res.statusMessage}`));
-monitor.on("stop", (website) => console.log(`${website} has stopped.`));
-monitor.on("error", (error) => console.log(error));
