442b3b551bb5cb0b599829a87895bc82fe1ede49
liqki <88623150+liqki@users.noreply.github.com>
2023-04-30 13:44:37 +0000

    fixed server status

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,29 +6,26 @@ module.exports = {
   async execute(message) {
     if (message.author.bot) return;
     if (message.author.id !== process.env.OWNER_ID) return;
-    if (message.mentions.users.first().id === process.env.CLIENT_ID && message.content.length === 22) {
-      message.channel.send("did I just hear my name? :flushed:");
-    }
     if (message.content === "!serverstatus") {
       const host = "liqkismp.feathermc.gg";
       const embed = new EmbedBuilder().setTitle("Checking server status...");
       message.channel.send({ embeds: [embed] }).then((msg) => {
         setInterval(() => {
-          
-        mcs
-          .statusJava(host)
-          .then((res) => {
-            const { online, players, version, icon } = res;
-            const embed = new EmbedBuilder()
-              .setTitle("Server Status")
-              .addFields({ name: "Online", value: online }, { name: "Players", value: players.online + "/" + players.max }, { name: "Version", value: version.name_clean })
-              .setThumbnail(icon)
-              .setColor(online ? "Green" : "Red");
-            msg.edit({ embeds: [embed] });
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+          mcs
+            .statusJava(host)
+            .then((res) => {
+              const { online, players, version, icon } = res;
+              console.log(online, players, version);
+              const embed = new EmbedBuilder()
+                .setTitle("Server Status")
+                .addFields({ name: "Online", value: online ? "yes" : "no" }, { name: "Players", value: players.online + "/" + players.max }, { name: "Version", value: version.name_clean })
+                .setThumbnail(icon)
+                .setColor(online ? "Green" : "Red");
+              msg.edit({ embeds: [embed] });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         }, 10000);
       });
     }
